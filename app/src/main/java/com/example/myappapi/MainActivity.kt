bd6dc94b851052c3591539bf6f5c8c6b36af5931
Legementarion <legementario357442829>
2021-07-10 21:50:53 +0300

    Initialization logic

--- app/src/main/java/com/example/myappapi/MainActivity.kt
+++ app/src/main/java/com/example/myappapi/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.myappapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import data.RemoteDataSourceImpl
 import repo.PostRepositoryImpl
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var mainPresenter: MainPresenter
-    lateinit var dataSource: RemoteDataSourceImpl
-    lateinit var comonRepo: PostRepositoryImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainPresenter =
+            MainPresenter(GetNewsListUseCase(PostRepositoryImpl(RemoteDataSourceImpl())))
+
         mainPresenter.getListNews()
     }
+
 }
